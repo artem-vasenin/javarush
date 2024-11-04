a176c69b37f341a3fcfcc9a8ba5cf3ea1850eff3
ElenaMinikhanova <elenaminikhanova89@gmail.com>
2024-11-04 19:19:37 +0700

    corrected version

--- elena/main.py
+++ elena/main.py
@@ -1,110 +1,77 @@
 from random import randrange
-# ToDo: отступы в 2 строки от импортов до основного кода
+
+
 attempt = 6
-letters_remains = ["а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с" , "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ь", "ы", "ъ", "э", "ю", "я"]
-slovo = None
-# ToDo: можно под одну крышу в словарь запаковать списки и дергать по ключу
+letters_remains = [chr(x) for x in range(1072, 1104)]
+words = None
 number_levels = ["Пять букв", "Шесть букв", "Семь букв", "Восемь букв"]
 five_letters = ["армия", "акула", "башня", "вафля", "газон", "гуашь"]
 six_letters = ["йогурт", "лстья", "червяк", "яблоко", "биолог", "бизнес", "гвоздь"] # опечатка лстья
 seven_letters = ["зеркало", "джекпот", "леопард", "подъезд", "дельфин"]
 eight_letters = ["авимбуве", "детектив", "агенство"]
-# ToDo: отступы между функциями
+list_letters = [five_letters, six_letters, seven_letters, eight_letters]
+dict_letters = {i+1:letter for i, letter in enumerate(list_letters)}
+
 def level_selection():
     for index, value in enumerate(number_levels):
         print(f"{index+1}. {value}")
-    # ToDo: отступы между блоками циклов
     while True:
         lvl = input(f"Выбирите уровень от 1 до {len(number_levels)}: ")
-        if lvl.isdigit() and 0 < int(lvl) <= len(number_levels):
+        if lvl.isdigit() and 0<int(lvl)<=len(number_levels):
             return game_levels(lvl)
         else:
             print(f"Введите цифру от 1 до {len(number_levels)}")
-# ToDo: отступы между функциями, большой if/else можно симпатишнее сделать словарем с ключом lvl
+
 def game_levels(lvl):
-    if int(lvl) == 1:
-        levels_one()
-    elif int(lvl) == 2:
-        levels_two()
-    elif int(lvl) == 3:
-        levels_three()
-    elif int(lvl) == 4:
-        levels_four()
-# ToDo: отступы между функциями, функции очень похожи значит можно попробовать сделать одну с параметрами
-def levels_one():
-    global slovo
-    index = randrange(1, len(five_letters))
-    slovo = five_letters[index]
-    game(slovo)
-
-def levels_two():
-    global slovo
-    index = randrange(1, len(six_letters))
-    slovo = six_letters[index]
-    game(slovo)
-
-def levels_three():
-    global slovo
-    index = randrange(1, len(seven_letters))
-    slovo = seven_letters[index]
-    return game(slovo)
-
-def levels_four():
-    global slovo
-    index = randrange(1, len(eight_letters))
-    slovo = eight_letters[index]
-    game(slovo)
-
+    global words
+    index = randrange(0, len(dict_letters[int(lvl)]))
+    words = dict_letters[int(lvl)][index]
+    game(words)
 
 def letters_remain():
     global letters_remains
     for i in letters_remains:
         print(i, end=" ")
 
-# ToDo: именование функций лучше на инглише не транслит
-def slov(slovo):
-    print("Ваше слово:", "*"*len(slovo))
+def found_letter(letters, text, list_words):
+    print(f"Вы угадали букву!")
+    letters_remains.remove(letters.lower())
+    while letters.lower() in text:
+        list_words[text.index(letters)] = letters
+        text[text.index(letters)] = None
+    if "*" not in list_words:
+        print(f"Вы выиграли! Было загадано слово: {words}")
+        exit = input("Нажмите Enter для выхода: ")
+        if exit=="":
+            "Пока"
+            return
 
-
-def game(slovo):
+def check_letter(text, list_words):
     global attempt
-    text = list(slovo)
-    list_slovo = ["*" for i in range(len(slovo))]
-    while attempt>0:
-    # ToDo: лишние отступы в блоке цикла
-
-        print(list_slovo)
+    while attempt>0 and "*" in list_words:
+        print(list_words)
         letters_remain()
         letters = input(f"\nВведите букву, у Вас оталось {attempt} попыток:")
         # ToDo: отступы между блоками условий и переменными
         if letters.lower() in letters_remains and letters.lower() in text:
-            print(f"Вы угадали букву!")
-            letters_remains.remove(letters.lower())
-            # ToDo: отступы между переменными и блоками циклов и условий
-            while letters.lower() in text:
-                list_slovo[text.index(letters)] = letters
-                text[text.index(letters)] = None
-            if "*" not in list_slovo:
-                print(f"Вы выиграли! Было загадано слово: {slovo}")
-                exit = input("Нажмите Enter для выхода: ")
-                if exit == "":
-                    "Пока"
-                    return
+            found_letter(letters, text, list_words)
         elif letters.lower() in letters_remains and letters.lower() not in text:
             print(f"Вы не угадали букву!")
             letters_remains.remove(letters.lower())
-            attempt -= 1
+            attempt-=1
         else:
             print(f"Введити букву из списка")
         if attempt<=0:
-            print(f'Вы проиграли. Было загаданно слово: "{slovo}"')
-            exit = input("Нажмите Enter для выхода: ")
-            if exit == "":
+            print(f'Вы проиграли. Было загаданно слово: "{words}"')
+            exit=input("Нажмите Enter для выхода: ")
+            if exit=="":
                 "Пока"
+                return
 
-# ToDo: большой отступ до проверки на тип вызываемого файла
-
-
+def game(words):
+    text = list(words)
+    list_words = ["*" for i in range(len(words))]
+    check_letter(text, list_words)
 
 if __name__ == "__main__":
     level_selection()
