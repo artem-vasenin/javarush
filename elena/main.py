aee6db9041359a18d0cba985b8619d48f71b132f
5ergey <akifyev.sergey@gmail.com>
2024-11-05 20:10:21 +0300

    Исправил ошибки, добавил docstring

--- elena/main.py
+++ elena/main.py
@@ -1,22 +1,22 @@
 from random import randrange
 
-
 attempt = 6
 letters_remains = [chr(x) for x in range(1072, 1104)]
 words = None
 number_levels = ["Пять букв", "Шесть букв", "Семь букв", "Восемь букв"]
 five_letters = ["армия", "акула", "башня", "вафля", "газон", "гуашь"]
-six_letters = ["йогурт", "лстья", "червяк", "яблоко", "биолог", "бизнес", "гвоздь"] # опечатка лстья
+six_letters = ["йогурт", "листья", "червяк", "яблоко", "биолог", "бизнес", "гвоздь"] # опечатка листья
 seven_letters = ["зеркало", "джекпот", "леопард", "подъезд", "дельфин"]
 eight_letters = ["авимбуве", "детектив", "агенство"]
 list_letters = [five_letters, six_letters, seven_letters, eight_letters]
 dict_letters = {i+1:letter for i, letter in enumerate(list_letters)}
 
 def level_selection():
+    '''Пользователь выбирает уровень'''
     for index, value in enumerate(number_levels):
         print(f"{index+1}. {value}")
     while True:
-        lvl = input(f"Выбирите уровень от 1 до {len(number_levels)}: ")
+        lvl = input(f"Выберите уровень от 1 до {len(number_levels)}: ")
         if lvl.isdigit() and 0<int(lvl)<=len(number_levels):
             return game_levels(lvl)
         else:
@@ -51,7 +51,7 @@ def check_letter(text, list_words):
     while attempt>0 and "*" in list_words:
         print(list_words)
         letters_remain()
-        letters = input(f"\nВведите букву, у Вас оталось {attempt} попыток:")
+        letters = input(f"\nВведите букву, у Вас осталось {attempt} попыток:")
 
         if letters.lower() in letters_remains and letters.lower() in text:
             found_letter(letters, text, list_words)
@@ -60,9 +60,9 @@ def check_letter(text, list_words):
             letters_remains.remove(letters.lower())
             attempt-=1
         else:
-            print(f"Введити букву из списка")
+            print(f"Введите букву из списка")
         if attempt<=0:
-            print(f'Вы проиграли. Было загаданно слово: "{words}"')
+            print(f'Вы проиграли. Было загадано слово: "{words}"')
             exit=input("Нажмите Enter для выхода: ")
             if exit=="":
                 print("Пока")
