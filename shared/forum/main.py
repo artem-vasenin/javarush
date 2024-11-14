2f687bd89a69488bb79cc5f68464db713ba44063
5ergey <akifyev.sergey@gmail.com>
2024-11-14 21:36:49 +0300

    Добавил функции Написал базово функции print_menu, register, choose_action

--- shared/forum/main.py
+++ shared/forum/main.py
@@ -1,5 +1,49 @@
+import hashlib
+import datetime
+import os.path
+
+def print_menu():
+    menu_options = {
+        1: 'Регистрация',
+        2: 'Аутентификация',
+        3: 'Просмотр списка пользователей',
+        4: 'Просмотр веток форума',
+        5: 'Выход'
+    }
+    for key, value in menu_options.items():
+        print(f'{key} ---- {value}')
+    user_choose = input("Выберите пункт меню: ")
+    return user_choose
+
+def choose_action():
+    user_choose = print_menu()
+    if user_choose == 1:
+        register()
+    elif user_choose == 2:
+        authentication()
+    elif user_choose == 3:
+        get_user_lists()
+    elif user_choose == 4:
+        print_branches()
+    elif user_choose == 5:
+        return None
+
 def register():
-    pass
+    """
+    Простая регистрация без проверки сложности пароля (пока)
+    """
+    login = input("Введите Ваше имя пользователя: ")
+    password = input("Введите Ваш пароль")
+    secretkey = input("Введите секретный ключ (для привилегированных пользователей): ")
+    hash = hashlib.md5(password.encode()).hexdigest()
+    if login != '' and hash !='':
+        if os.path.exists(f'/home/forum/users/{login}'):
+            print(f'Имя пользователя {login} уже существует, придумайте другое')
+        else:
+            with open(f'/home/forum/users/{login}', 'w', encoding="utf-8") as file:
+                file.write(login)
+                file.write(hash)
+
 
 def authentication():
     pass
@@ -37,5 +81,14 @@ def unblock_users():
 def delete_users():
     pass
 
+def get_user_lists():
+    pass
+
+def print_branches():
+    pass
+
 def hacker():
     pass
+
+
+print_menu()
