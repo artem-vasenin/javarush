7797843e68fed3c0c2c8ac2c126dbf8ccb3a069b
5ergey <akifyev.sergey@gmail.com>
2024-11-14 20:16:50 +0300

    Core functions

---
+++ shared/forum/main.py
@@ -0,0 +1,41 @@
+def register():
+    pass
+
+def authentication():
+    pass
+
+def authorization():
+    pass
+
+def listing_branch():
+    pass
+
+def listing_themes():
+    pass
+
+def create_branch():
+    pass
+
+def create_themes():
+    pass
+
+def create_forum_messages():
+    pass
+
+def create_personal_messages():
+    pass
+
+def bot():
+    pass
+
+def block_users():
+    pass
+
+def unblock_users():
+    pass
+
+def delete_users():
+    pass
+
+def hacker():
+    pass
